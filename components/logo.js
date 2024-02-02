99157b30afeeb7cfabf880ea7464170e38c6892b
Sushi <158117453+nlam0@users.noreply.github.com>
2024-02-02 16:31:53 -0500

    bugfix4

--- components/logo.js
+++ components/logo.js
@@ -22,7 +22,7 @@ const LogoBox = styled.span`
 `
 
 const Logo = () => {
-  const footPrintImg = `/images/puzzleicon${useColorModeValue('', '-dark')}.png`
+  const footPrintImg = `/images/puzzle-icon${useColorModeValue('', '-dark')}.png`
   return (
     (<Link href="/" scroll={false}>
 

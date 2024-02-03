64a4bf4637d6f4ee558944b77b328734534cca2a
Sushi <158117453+nlam0@users.noreply.github.com>
2024-02-02 20:11:38 -0500

    favicon2

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -18,8 +18,11 @@ const Main = ({ children, router }) => {
         <meta name="description" content="Nick's homepage" />
         <meta name="author" content="Nick Lam" />
         <meta name="author" content="nlam0" />
-        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
         <meta name="twitter:title" content="Nick Lam" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@craftzdog" />

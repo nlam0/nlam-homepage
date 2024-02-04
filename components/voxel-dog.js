396bd0db1791b90a359288b419dad772c60bdc7c
Sushi <158117453+nlam0@users.noreply.github.com>
2024-02-04 03:16:06 -0500

    camera

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -43,7 +43,7 @@ const VoxelDog = () => {
       refRenderer.current = renderer
       const scene = new THREE.Scene()
 
-      const target = new THREE.Vector3(-0.5, 1.2, -1)
+      const target = new THREE.Vector3(-2.5, 1.2, -2)
       const initialCameraPosition = new THREE.Vector3(
         20 * Math.sin(0.2 * Math.PI),
         10,
